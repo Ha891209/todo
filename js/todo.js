9e6d0f57bdb4408e8db85957f986a7ffa06450c1
Aniko Kopis-Horvath <ha891209@gmail.com>
2020-12-13 22:44:19 +0100

    HTML, CSS OK, JS 90%

--- js/todo.js
+++ js/todo.js
@@ -1,118 +1,184 @@
-(function() {
-    // Mock data.
-    let todos = [];
-    
-    // Parts of date.
-    const bodyDay = document.querySelector('.body__day');
-    const bodyDate = document.querySelector('.body__date');
-    const todoAddBtn = document.querySelector('.todo__btn');
-    const todoInput = document.querySelector('.todo__input');
-    const todoListPending = document.querySelector('.todo__list--pending');
-    
-    const dayNames = [
-        'Sunday', 
-        'Monday', 
-        'Tuesday', 
-        'Wednessday', 
-        'Thursday', 
-        'Friday', 
-        'Saturday',
-    ];
+let pendingItems = 0;
+let completedItems = 0;
+const showButton = document.querySelector('.show__button');
+document.querySelector('.add__button').addEventListener('click', newListItem);
+showButton.addEventListener('click', showComplete);
+document.querySelector('.clear__button').addEventListener('click', clearAll);
 
-    // Localstorage handler object.
-    const localDB = {
-        // localDB.setItem('todos', todos);
-        setItem(key, value) {
-            value = JSON.stringify(value);
-            localStorage.setItem(key, value);
-        },
-        // localDB.getItem('todos')
-        getItem(key) {
-            const value = localStorage.getItem(key);
-            if (!value) {
-                return null;
-            }
+const addDate = () => {
+    const today = new Date();
+    const dayDisplayed = document.querySelector('.current__day');
+    const dateDisplayed = document.querySelector('.current__date');
+    let weekday = today.toLocaleString('en-US', { weekday: "long" })
+    let date = today.toLocaleDateString('en-US');
+    dayDisplayed.textContent = weekday;
+    dateDisplayed.textContent = date;
+}
 
-            return JSON.parse(value);
-        },
-        // localDB.removeItem('todos');
-        removeItem(key) {
-            localStorage.removeItem(key);
+addDate()
+
+const updatePending = () => {
+    const pending = document.querySelector('.pending__items');
+    if (pendingItems === 0) {
+        pending.textContent = `
+                    🍻
+        Time to chill! You have no todos.`
+    }
+    else {
+        pending.textContent = `You have ${pendingItems} pending tasks(s).`
+    }
+}
+
+const updateCompleted = () => {
+    const completed = document.querySelector('.completed__tasks');
+    completed.textContent = `You have ${completedItems} completed tasks(s).`
+}
+
+const checkBox = (ev) => {
+    if (ev.currentTarget.checked === true) {
+        ev.currentTarget.parentElement.parentElement.removeChild(ev.currentTarget.parentElement);
+        createTodoDoneDiv(ev);
+        done += 1;
+        percent = done / sorszam;
+        numberSet(-1);
+        doneCounter.textContent = (percent * 100).toPrecision(3);
+    }
+}
+
+const checkStorage = () => {
+    let tasks = [];
+    Object.keys(localStorage).forEach(function(key) {
+        tasks.push(key);
+     });
+    if (tasks !== []) {
+        for (let i=0; i<tasks.length; i++) {
+            const li = document.createElement("li");
+            const checkBox = document.createElement("input");
+            const closeButton = document.createElement("i");
+            const text = document.createElement('span');
+            text.textContent = tasks[i];
+            text.classList.add('list__text');
+            checkBox.type = 'checkbox';
+            checkBox.classList.add('list__checkbox');
+            closeButton.classList.add('close__button', 'fa', 'fa-trash');
+            itemGenerator(li, checkBox, text, closeButton);
+            listenerGenerator(li, checkBox, text, closeButton);
+            pendingItems++
+            updatePending();
         }
-    };
+    }
+    updatePending();
+}
 
-    // Initialize application.
-    const init = () => {
-        showDate();
-        setListeners();
-        loadExistingTodos();
-    };
+checkStorage();
 
-    // Load existing todos.
-    const loadExistingTodos = () => {
-        const savedTodos = localDB.getItem('todos');
-        if (savedTodos) {
-            todos = savedTodos;
+function textGenerator() {
+    const text = document.createElement('span');
+    let inputValue = document.querySelector(".input__field").value;
+    if (inputValue === '') {
+        inputValue = document.querySelector(".input__field").placeholder;
+    }
+    text.textContent = inputValue;
+    text.classList.add('list__text');
+    return text;
+}
+
+function itemGenerator(li, checkBox, text, closeButton) {
+    li.appendChild(checkBox);
+    li.appendChild(text);
+    li.appendChild(closeButton);
+    document.querySelector(".list").appendChild(li);
+    setTimeout(function () {
+        li.classList.add("show");
+    }, 10);
+    localStorage.setItem(text.textContent, text);
+}
+
+function notDuplicate(text) {
+    const list = document.querySelector(".list").children;
+    for (let i = 0; i < list.length; i++) {
+        if (text.textContent === (list[i].children[1]).textContent) {
+            return false;
         }
-        
-        if (todos && Array.isArray(todos)) {
-            todos.forEach( todo => showTodo(todo) );
+    }
+    return true;
+}
+
+function removeItem(li, text) {
+    li.removeChild(li.lastChild);
+    document.querySelector(".list").removeChild(li);
+    localStorage.removeItem(text.textContent);
+    pendingItems = pendingItems - 1;
+    updatePending();
+}
+
+function addCompletedItem(li) {
+    completedItems++;
+    updateCompleted();
+    document.querySelector(".list__completed").appendChild(li);
+}
+
+function listenerGenerator(li, checkBox, text, closeButton) {
+
+    li.addEventListener('mouseover', () => {
+        closeButton.classList.add('show');
+    });
+    li.addEventListener('mouseout', () => {
+        closeButton.classList.remove('show');
+    });
+
+    closeButton.addEventListener('click', () => {
+        removeItem(li, text);
+    });
+
+    checkBox.addEventListener('click', () => {
+        removeItem(li, text);
+        addCompletedItem(li);
+    });
+
+}
+
+function newListItem() {
+
+    const li = document.createElement("li");
+    const checkBox = document.createElement("input");
+    const closeButton = document.createElement("i");
+    checkBox.type = 'checkbox';
+    checkBox.classList.add('list__checkbox');
+    closeButton.classList.add('close__button', 'fa', 'fa-trash');
+    let text = textGenerator();
+    if (notDuplicate(text)) {
+        itemGenerator(li, checkBox, text, closeButton);
+        listenerGenerator(li, checkBox, text, closeButton);
+
+        pendingItems++
+        updatePending();
+    }
+    document.querySelector(".input__field").value = "";
+}
+
+function clearAll() {
+    const list1 = document.querySelector(".list__completed");
+    while (list1.firstChild) {
+        list1.removeChild(list1.lastChild);
+    }
+    completedItems = 0;
+    showComplete();
+
+}
+
+function showComplete() {
+    const completed = document.querySelector('.completed__tasks');
+    const list = document.querySelector(".list__completed");
+    if (showButton.textContent === 'Show Complete') {
+        if (completedItems > 0) {
+            completed.classList.add('show');
+            list.classList.add('show');
+            showButton.textContent = 'Hide Complete';
         }
-    };
-
-    // Show date.
-    const showDate = () => {
-        const currentDate = new Date();
-        const day = [
-            currentDate.getMonth() + 1, 
-            currentDate.getDate(),
-            currentDate.getFullYear(), 
-        ].map( num => num < 10 ? `0${num}` : num );
-
-        bodyDay.textContent = dayNames[currentDate.getDay()];
-        bodyDate.textContent = day.join('-');
-    };
-
-    // Set event listeners.
-    const setListeners = () => {
-        todoAddBtn.addEventListener('click', addNewTodo);
-    };
-
-    // Save and add todo to the database.
-    const addNewTodo = () => {
-        const value = todoInput.value;
-        if (value === '') {
-            alert('Please type a todo.');
-            return;
-        }
-
-        const todo = {
-            text: value,
-            done: false
-        };
-
-        todos.push(todo);
-
-        localDB.setItem('todos', todos);
-
-        showTodo(todo);
-
-        todoInput.value = '';
-    };
-
-    // Show todo in the list.
-    const showTodo = todo => {
-        const todoItem = document.createElement('div');
-        todoListPending.appendChild(todoItem);
-
-        todoItem.innerHTML = `
-            <input type="checkbox">
-            <span>${todo.text}</span>
-            <button>
-                <i class="fa fa-trash"></i>
-            </button>        
-        `;
-    };
-
-    init();
-})();
+    } else {
+        completed.classList.remove('show');
+        list.classList.remove('show');
+        showButton.textContent = 'Show Complete';
+    }
+}
